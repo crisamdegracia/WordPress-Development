d2b4c8b1398b0ec98fad814cad605dd72c9a66ce
Crisam De Gracia <crisamdegracia@gmail.com>
2020-08-30 03:33:17 +0800

    end in:f5v17 | Menu of child page links

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -16,19 +16,59 @@ get_header();
 
 <div class="container container--narrow page-section">
 
+    <?php
+
+    $parentPageID  = wp_get_post_parent_id( get_the_ID() );
+
+
+         if (  $parentPageID  ) { ?>
+
     <div class="metabox metabox--position-up metabox--with-home-link">
-        <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+        <p><a class="metabox__blog-home-link" href="<?php echo get_the_permalink( $parentPageID) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title( $parentPageID ) ?></a> <span class="metabox__main"><?php the_title() ?></span></p>
+
     </div>
 
-<!--
+
+    <?php
+                                }
+    ?>
+
+
+    <?php 
+
+    $testArray = get_pages(array(
+        'child_of'  => get_the_ID()
+    ));
+
+    if( $parentPageID or $testArray ) { ?>
+
     <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+        <h2 class="page-links__title"><a href="<?php echo get_the_permalink( $parentPageID ) ?>"><?php echo get_the_title($parentPageID) ?></a></h2>
         <ul class="min-list">
-            <li class="current_page_item"><a href="#">Our History</a></li>
-            <li><a href="#">Our Goals</a></li>
+
+            <?php 
+
+        if( $parentPageID ){
+            $findChildrenOf = $parentPageID;
+        } else {
+            $findChildrenOf = get_the_ID();
+        }
+
+                                       wp_list_pages( array(
+                                           //title_li is empty 
+                                           //child_of - numerical ID of a certain page or post
+                                           //sort_column => 'menu_order' we can choose the order output of link -
+                                           // ^ you can control this on right panel and choose the order number
+                                           'title_li' => NULL,
+                                           'child_of' => $findChildrenOf,
+                                           'sort_column' => 'menu_order'
+
+
+                                       ))   ?>
+
         </ul>
     </div>
--->
+    <?php } ?>
 
     <div class="generic-content">
         <p><?php the_content(); ?></p>
